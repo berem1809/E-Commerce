dd2b1bc4c352e9d501c3eb25329cc771d930f553
Piranaavei Beremkumar <108848056+berem1809@users.noreply.github.com>
2024-08-27 19:42:00 +0530

    Some More Modifications

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import ShopCategory from './Pages/ShopCategory';
 import Footer from './Components/Footer/Footer';
 import men_banner from './Components/Assets/banner_mens.png'
 import women_banner from './Components/Assets/banner_women.png'
-import kids_banner from './Components/Assets/banner_kids.png'
+import kid_banner from './Components/Assets/banner_kids.png'
 
 
 
@@ -19,20 +19,18 @@ const App = () => {
      <BrowserRouter>
      <Navbar/>
      <Routes>
-      
-
         <Route path="/" element={<Shop/>}/>
-        <Route path="/men" element={<ShopCategory banner={ men_banner }ategory ="men"/>}/>
+        <Route path="/men" element={<ShopCategory banner={ men_banner } category ="men"/>}/>
         <Route path="/women" element={<ShopCategory  banner={ women_banner } category ="women"/>}/>
-        <Route path="/kids" element={<ShopCategory banner={ kids_banner } category ="kids"/>}/>
+        <Route path="/kids" element={<ShopCategory banner={ kid_banner } category ="kid"/>}/>
         <Route path ="/product" element = {<Product/>}>
         <Route path=':productId' element ={<Product/>}/>
         </Route>
         <Route path="/cart" element={<Cart/>}/>
         <Route path="/login" element={<LoginSignUp/>}/>
-     </Routes>
-     <Footer/>
-     </BrowserRouter></div>
+        </Routes>
+        <Footer/>
+        </BrowserRouter></div>
   )
 }
 
--- src/Pages/LoginSignUp.jsx
+++ src/Pages/LoginSignUp.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
+import './CSS/LoginSignup.css'
 
 const LoginSignUp = () => {
   return (
-    <div>LoginSignUp</div>
+    <div className="loginsignup">
+      <div className="loginsignup-container">
+        <h1>
+          Sign Up
+        </h1>
+        <div className="loginsignup-fields">
+          <input type="text" placeholder="Username"/>
+          <input type="text" placeholder="Email"/>
+          <input type="password" placeholder="Password"/>
+          </div>
+          <button>Continue</button>
+          <p classNme ="loginsignup-login">Already have an account? <span>Login here</span></p>
+           <div className = 'loginsignup-agree'>
+            <input type="checkbox" name="" id="" />
+            <p>By continuing, i agree to the terms of use & privacy policy.</p>
+           </div>
+        </div>
+      </div>
+    
   )
 }
 
